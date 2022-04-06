10125d1117a0d9d6d6cfe1da84862a50c4c99adf
Dillion Megida <dillionmegida@gmail.com>
2022-04-06 11:35:59 +0200

    feat: add demo users

---
+++ src/users.js
@@ -0,0 +1,28 @@
+const users = [
+  {
+    id: 'mike',
+    name: 'Mike',
+    image:
+      'https://i.picsum.photos/id/92/200/200.jpg?hmac=2cxZLFe94hVFQL5AERTDzRKET_GDG-2qpFi5-ctPekg',
+  },
+  {
+    id: 'mary1',
+    name: 'Mary',
+    image:
+      'https://i.picsum.photos/id/646/200/200.jpg?hmac=3jbia15y-hA5gmqVJjmk6BPJiisi4j-fNKPi3iXRiRo',
+  },
+  {
+    id: 'jake',
+    name: 'Jake',
+    image:
+      'https://i.picsum.photos/id/124/200/200.jpg?hmac=FuA4HgovVpaMlT_5gnjY_28jYCrrA2xrYXy3mJ9XDEw',
+  },
+  {
+    id: 'joe',
+    name: 'Joe',
+    image:
+      'https://i.picsum.photos/id/703/200/200.jpg?hmac=6zWxIBRmIf2e0jZTqvKBIwrc7wm-dPkvGky4go6Yyvg',
+  },
+]
+
+export default users
